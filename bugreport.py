fa37185c40007910a3e2ec5d39d3f72ba6a21ffb
Dmytro Kaliazin <dmitry.kalyazin@gmail.com>
2015-10-09 15:48:58 +0000

    bugreport and bugtracker classes updated

--- bugreport.py
+++ bugreport.py
@@ -1,23 +1,33 @@
+
 class BugReport:
-"""Particular bug report class, contain all mandatory attributes and methods"""
+    """Particular bug report class, contain all mandatory attributes and methods"""
     bug_id = 0
     time_open = 0
-    bug_status = none
-    opened_by = none
-    time_fixed = none
-    assigned_to = none
-    crated_by = none
-    descripotion = none
-    title = none
-
-"""Set current bug report status"""   
+    bug_status = ''
+    opened_by = ''
+    time_fixed = ''
+    assigned_to = ''
+    crated_by = ''
+    descripotion = ''
+    title = ''
+    def __init__(self,report_title,report_description):
+        self.title = report_title
+        self.descripotion = report_description
+        #time_open = now
+        #crated_by = who 
+    """Set current bug report status"""   
     def set_status(self, status):
-"""Assign bog report to some person( assigted to bug creator by default)"""
+        pass
+    """Assign bog report to some person( assigted to bug creator by default)"""
     def assign_to(self):
-"""Run editor and save Description of bug"""
+        pass
+    """Run editor and save Description of bug"""
     def set_description(self):
-"""Set report title"""
+        pass
+    """Set report title"""
     def set_title(self):
-"""Add comment"""
+        pass
+    """Add comment"""
     def add_comment(self):
+        pass
 
--- bugtracker.py
+++ bugtracker.py
@@ -1,5 +1,24 @@
+import pickle
+from  bugreport import BugReport
+ 
 class BugTracker:
-    bug_reports
+    bug_reports = []
+    def __init__(self):
+        #reports_file = open("database/reports.txt","r")
+        pass
+
     def create_new_report(self):
+        #reports_file = open("database/reports.txt","w")
+
+        report_title = raw_input("Please, enter bugreport title:")
+        report_desc = raw_input("Please, enter bugreport description:")
+   
+        report = BugReport(report_title,report_desc)
+        self.bug_reports.append(report)
+        
+    
     def search_reports(self):
+        pass
     def list_reports(self):
+        for report in self.bug_reports:
+            print(report.bug_id,"\t",report.title)
